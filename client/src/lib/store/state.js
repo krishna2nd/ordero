c8fbe6fade4a471658eac1822855e561958e7fd7
Krishna <krishna2nd@gmail.com>
2017-11-04 14:44:01 +0530

    Update in pack system and react to 16

--- client/src/lib/store/state.js
+++ client/src/lib/store/state.js
@@ -56,7 +56,7 @@ function r() {
 
 const randomId = () => randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
 let counter = 1;
-const seq = () => counter + 1
+const seq = () => counter++
 
 function randomItem() {
 	const cost =Math.floor(Math.random()*500);
@@ -64,10 +64,11 @@ function randomItem() {
 	const sCharge = (cost * .08).toFixed(2);
 	const price = (cost+(+tax)+(+sCharge)).toFixed(2);
 	const objCategory = categories[Math.floor(Math.random(categories.length)*categories.length)]
+	const id = `item-${seq()}`
 	const item = {
-		id: 'item-' + seq(), 
+		id: id,
 		category: objCategory.category,
-		title: "Item name in detail log its some dummy",
+		title: `Item ${id} title `,
 		info: `It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.`,
 		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
 
